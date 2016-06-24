7621bb2e80c1111fd47b2d8778380af4cd57b666
Jesus Larios Murillo <jlariosmurillo@mesosphere.io>
2016-06-24 14:53:17 -0700

    scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,19 +12,18 @@ echo 'Installing curl ...'
 yum install -y curl
 
 echo 'Installing Java ...'
-yum install -y open java-1.8.0-openjdk
+yum install java-1.8.0-openjdk-devel
 javac -version
 
 echo 'Installing Scala ...'
 scala="scala-2.11.8"
 wget http://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.tgz
 tar xvf ${scala}.tgz
-sudo mv $scala /usr/lib
-sudo ln -s /usr/lib/$scala /usr/lib/scala
-export PATH=$PATH:/usr/lib/scala/bin
-scala -version
+mv $scala /usr/lib
+ln -s /usr/lib/$scala /usr/lib/scala
 
 echo 'Installing Sbt ...'
+mkdir ~/bin
 curl -s https://raw.githubusercontent.com/paulp/sbt-extras/master/sbt > ~/bin/sbt \
   && chmod 0755 ~/bin/sbt
 
