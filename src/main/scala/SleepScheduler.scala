2c4d48b2f7edcd27ea44a7b736ea1df69c59af53
Jesus Larios Murillo <jlariosmurillo@mesosphere.io>
2016-06-27 11:02:54 -0700

    bugs

--- src/main/scala/SleepScheduler.scala
+++ src/main/scala/SleepScheduler.scala
@@ -13,25 +13,25 @@ class SleepScheduler extends Scheduler {
 
   val log = Logger.getLogger(getClass.getName)
 
-  override def offerRescinded(driver: SchedulerDriver, offerId: OfferID): Unit = {}
+  override def offerRescinded(driver: SchedulerDriver, offerId: OfferID): Unit =
     log.info(s"offerRecinded: Offer ${offerId.getValue} has been rescinded")
 
   override def disconnected(driver: SchedulerDriver): Unit = {}
     log.info("disconnected: Disconnected from the mesos master")
 
-  override def reregistered(driver: SchedulerDriver, masterInfo: MasterInfo): Unit = {}
+  override def reregistered(driver: SchedulerDriver, masterInfo: MasterInfo): Unit =
     log.info("reregistered: Reregistered with the mesos master")
 
-  override def slaveLost(driver: SchedulerDriver, slaveId: SlaveID): Unit = {}
+  override def slaveLost(driver: SchedulerDriver, slaveId: SlaveID): Unit =
     log.info(s"slaveLost: Slave ${slaveId.getValue} lost :(")
 
-  override def error(driver: SchedulerDriver, message: String): Unit = {}
+  override def error(driver: SchedulerDriver, message: String): Unit =
     log.info(s"error: Error: $message")
 
-  override def statusUpdate(driver: SchedulerDriver, status: TaskStatus): Unit = {}
+  override def statusUpdate(driver: SchedulerDriver, status: TaskStatus): Unit =
     log.info(s"statusUpdate: Status update: Task ${status.getTaskId.getValue} is in state ${status.getState}")
 
-  override def frameworkMessage(driver: SchedulerDriver, executorId: ExecutorID, slaveId: SlaveID, data: Array[Byte]): Unit = {}
+  override def frameworkMessage(driver: SchedulerDriver, executorId: ExecutorID, slaveId: SlaveID, data: Array[Byte]): Unit =
     log.info(s"frameworkMessage: Received message from executor ${executorId.getValue} at slave ${slaveId.getValue} with contents ${data}")
 
   override def resourceOffers(driver: SchedulerDriver, offers: util.List[Offer]): Unit = {
@@ -63,9 +63,9 @@ class SleepScheduler extends Scheduler {
     }
   }
 
-  override def registered(driver: SchedulerDriver, frameworkId: FrameworkID, masterInfo: MasterInfo): Unit = {}
+  override def registered(driver: SchedulerDriver, frameworkId: FrameworkID, masterInfo: MasterInfo): Unit =
     log.info(s"registered: Registered with mesos master ${masterInfo.getId} at ip ${masterInfo.getIp} with port ${masterInfo.getPort}")
 
-  override def executorLost(driver: SchedulerDriver, executorId: ExecutorID, slaveId: SlaveID, status: Int): Unit = {}
+  override def executorLost(driver: SchedulerDriver, executorId: ExecutorID, slaveId: SlaveID, status: Int): Unit =
     log.info(s"executorLost: We lost the executor ${executorId.getValue} at slave ${slaveId.getValue}!!!")
 }
